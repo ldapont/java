8bea44844e6584f3ad9dc6018cf1ed3ccc205121
ldapont <ldapontpoa@gmail.com>
2024-08-18 16:06:21 -0300

    Add files via upload

---
+++ LoopFor.java
@@ -0,0 +1,28 @@
+package com.loiane.cursojava.aula17;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<5; i++ ) {
+            System.out.println("O valor de i é: " + i);
+	}
+		for(int i=0, j=10; i<j ; i++, j--) {
+            System.out.println("i = " + i + "; j = " + j);
+}
+		int count = 0;
+		for(; count<10 ;) {
+			System.out.println("Valor de count é: " + count);
+			count += 2;
+		}
+		
+		for (int cont=0 ; cont < 10 ; cont +=2) {
+			System.out.println("Valor de cont é: " + cont);
+		}
+		
+		int soma = 0;
+		for (int i = 1; i < 5; soma += i++); {
+			System.out.println("O valor da soma é: " + soma);
+		}
+}
+}
---
+++ Variaveis.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula11;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 49;
+		String nome = "Luciano";
+		String nomeDoMeuCachorro = "Lalo"; 
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("Nome do meu cachorro: " + nomeDoMeuCachorro);
+	}
+
+}
