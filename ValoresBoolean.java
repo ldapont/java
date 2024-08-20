93c9c30fb3e2c10feb439eb9d2f5b09b3118ac02
ldapont <ldapontpoa@gmail.com>
2024-08-20 20:04:32 -0300

    Add files via upload

---
+++ ValoresBoolean.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula10;
+
+public class ValoresBoolean {
+
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;
+		
+		boolean falso = false;
+		
+		System.out.println("O valor de verdadeiro é: " + verdadeiro);
+		System.out.println("O valor de falso é: " + falso);
+
+	}
+
+}
