581e323ea7519475b1677bf5c0e5d1090b310a8b
ldapont <ldapontpoa@gmail.com>
2024-08-30 19:46:15 -0300

    Add files via upload

---
+++ Pacote.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula28;
+
+import com.loiane.cursojava.aula24.Carro;
+import com.loiane.cursojava.exercicios.exerc001;
+import com.loiane.cursojava.exercicios.exerc002;
+
+public class Pacote {
+
+	public static void main(String[] args) {
+		
+		Carro carro;
+		com.loiane.cursojava.aula25.Carro2 carro2;
+		
+		exerc001 exerc001;
+        exerc002 exerc002;
+	}
+
+}
