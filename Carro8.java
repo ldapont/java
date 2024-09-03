7fa16367c2a28352b874d916456a126825afbbd1
ldapont <ldapontpoa@gmail.com>
2024-09-03 19:36:28 -0300

    Add files via upload

---
+++ Carro8.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula32;
+
+public class Carro8 {
+	
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capCombustivel;
+	private double consumoCombustivel; 
+	
+    public String getMarca( ) {
+    	
+    	return this.marca;
+    }
+    
+    public void setMarca(String marca) {
+    	
+    	this.marca = marca;
+    }
+}
---
+++ TesteCarro8.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula32;
+
+public class TesteCarro8 {
+
+	public static void main(String[] args) {
+		
+		Carro8 van = new Carro8();
+		
+		van.setMarca("Fiat");
+		
+		System.out.println(van.getMarca());
+
+	}
+
+}
