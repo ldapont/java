7e3a9c90de1cb8c28168e0a6956c6d44b0abd576
ldapont <ldapontpoa@gmail.com>
2024-09-05 14:23:32 -0300

    Add files via upload

---
+++ TesteCalculadora2.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula34;
+
+
+public class TesteCalculadora2 {
+
+		
+		public static void main(String[] args) {
+
+			
+			//MinhaCalculadora2 calc = new MinhaCalculadora2(); 
+			
+			MinhaCalculadora2.soma(1, 2);
+
+		}
+
+	}
+
+
