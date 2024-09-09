f44f27f2a3b37d8c6474f5cce16ecab9d9d53f7c
ldapont <ldapontpoa@gmail.com>
2024-09-09 20:49:31 -0300

    Add files via upload

---
+++ TesteEscola.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula37;
+
+public class TesteEscola {
+	
+	public static void main (String[] args ) {
+		
+		Aluno aluno = new Aluno();
+		
+		Professor professor = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		aluno.setNome("Maria");
+		professor.setCpf("111.111.111-11");
+		professor.setTelefoneCelular("11 99999 9999");
+		
+		
+		
+		
+	}
+	
+	
+
+}
